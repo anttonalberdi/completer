5a7bb56b6c991aca2f49e968e7d9f4fe752a7c25
anttonalberdi <anttonalberdi@gmail.com>
2024-09-03 18:21:05 +0200

    setup update

--- R/setup_completer.R
+++ R/setup_completer.R
@@ -4,9 +4,11 @@
 #' @export
 
 setup_completer <- function(){
-  message("Downloading 5.6GB of reference data This will take a while...")
+  message("Downloading 5.6GBs of reference data.")
+  message("  This will take a while...")
   data_path <- file.path(system.file("exdata", package = "completer"), "data.RData")
   curl::curl_download("https://sid.erda.dk/share_redirect/H9VSThONVr/data.RData",data_path, quiet = FALSE)
+  message("  Download finished succesfully. Loading objects to environment.")
   load(data_path, envir = .GlobalEnv)
 }
 
--- R/zzz.R
+++ R/zzz.R
@@ -3,5 +3,7 @@
   
   if (!file.exists(data_path)) {
     message("Reference data have not been downloaded yet. Run completer_setup() to get the library ready to run.")
+  } else {
+    load(data_path, envir = asNamespace(pkgname))
   }
 }
