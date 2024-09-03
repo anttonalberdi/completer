953c9d8ef0476d0b9025058d58c933bd05e1f20d
anttonalberdi <anttonalberdi@gmail.com>
2024-09-03 18:54:28 +0200

    Setup update

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  data_path <- file.path(system.file("exdata", package = "completer"), "data.RData")
+  data_path <- file.path(system.file("exdata", package = "completer"), "completer_v1.RData")
   
   if (!file.exists(data_path)) {
     message("Reference data have not been downloaded yet. Run completer_setup() to get the library ready to run.")
